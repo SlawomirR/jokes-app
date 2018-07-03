9526c121f65d71cf5f351c2745c901be2ce7995e
SlawomirR <no.for.spam@wp.pl>
2018-07-03 11:38:13 +0200

    Created Spring MVC Jokes controller

---
+++ src/main/java/com/joke/jokeapp/controllers/JokeController.java
@@ -0,0 +1,25 @@
+package com.joke.jokeapp.controllers;
+
+import com.joke.jokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class JokeController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/")
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}
