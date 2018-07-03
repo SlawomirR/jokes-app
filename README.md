0a95ee6a98eb8f54fc7a58a3918f505c401cc5d2
SlawomirR <no.for.spam@wp.pl>
2018-07-03 11:09:53 +0200

    Created project structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# Spring Boot Jokes App
+This Spring Boot web application displays a random joke.
---
+++ src/main/java/com/joke/jokeapp/JokeApplication.java
@@ -0,0 +1,12 @@
+package com.joke.jokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokeApplication.class, args);
+    }
+}
